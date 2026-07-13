0c0f9cb81b3ad5dd6a27c1b5ee961dd05a52f7b3
Markus Wenzel <wenzel1983@gmail.com>
2026-07-13 22:58:20 +0200

    feat: Selbsttest-Orchestrierung + Settings-Tab + Plugin-Verdrahtung

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,29 @@
 import { Plugin } from "obsidian";
+import { VaultbridgeSettingsTab } from "./ui/SettingsTab";
+
+export interface VaultbridgeSettings {
+  setupString: string;
+  deviceName: string;
+}
+
+const DEFAULT_SETTINGS: VaultbridgeSettings = {
+  setupString: "",
+  deviceName: "",
+};
 
 export default class VaultbridgePlugin extends Plugin {
+  settings: VaultbridgeSettings = DEFAULT_SETTINGS;
+
   async onload(): Promise<void> {
-    // Verdrahtung folgt in Task 6.
+    await this.loadSettings();
+    this.addSettingTab(new VaultbridgeSettingsTab(this.app, this));
+  }
+
+  async loadSettings(): Promise<void> {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings(): Promise<void> {
+    await this.saveData(this.settings);
   }
 }
